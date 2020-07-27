42ccbbc56f74fae3ae17b80ab371545c7cdbc930
Manjunath Koralli <mskoralli@gmail.com>
2020-07-27 18:23:07 +0530

    JS basics-3

--- JS - Basics/script.js
+++ JS - Basics/script.js
@@ -1,20 +1,30 @@
 // BASICS
 var firstName = "Manjunath";
 console.log(firstName);
-
 var lastName = "Koralli";
 var age = 22;
-
 console.log(firstName + " " + lastName + " age is " + age);
 var fullAge = true;
 console.log(fullAge);
 
-
 var job;
 //undefined - non existence
 console.log(job);
-
 job = "Programmer";
 console.log(job);
 
-// VARIABLE MUTATION AND TYPE COERCION
+//variable naming rules
+//valid
+var _3years = 3;
+//not valid
+//"error is" -  unexpected token if
+//var if = "if";
+
+/*
+    - VARIABLE MUTATION AND TYPE COERCION
+*/
+
+var firstName1 = "Manjunath";
+var age1 = 23;
+console.log(firstName1 + ' ' + age1);
+
