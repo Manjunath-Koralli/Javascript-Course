81853880b478a79f2a332b3803d77495ff6accf1
Manjunath Koralli <mskoralli@gmail.com>
2020-07-31 18:26:19 +0530

    Added If-else

--- Coding Challenge-1/script.js
+++ Coding Challenge-1/script.js
@@ -7,3 +7,11 @@ var BMIMark = markMass / ( markHeight * markHeight );
 
 var higherBMI = BMIMark > BMIJohn;
 console.log("Is Mark's BMI higher the John's BMI ? " + higherBMI);
+
+//update after conditional statement video
+if(BMIMark > BMIJohn){
+    console.log("Mark's BMI higher the John's BMI");
+}
+else {
+    console.log("John's BMI higher the Mark's BMI");
+}
---
+++ JS - Basics/script2.js
@@ -0,0 +1,22 @@
+/* 
+    Conditional Statements
+*/
+var firstName1 = "John";
+var firstName2 = "Mark";
+var civilStatus = "single";
+var isMarried = true;
+
+if (civilStatus === 'married'){
+    console.log(firstName1 + ' is married');
+}
+else {
+    console.log(firstName1 + ' will marry soon');
+}
+
+if (isMarried){
+    console.log(firstName2 + ' is married');
+}
+else {
+    console.log(firstName2 + ' will marry soon');
+}
+
