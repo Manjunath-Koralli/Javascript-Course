f7080da07f227d2b5ab90e58cb677263192d0219
Manju <mskoralli@gmail.com>
2021-06-14 12:36:31 +0530

    Challenge 14.2

--- Coding Challenge - 14.2/script.js
+++ Coding Challenge - 14.2/script.js
@@ -13,3 +13,36 @@ DATA CAR 1: 'Ford' going at 120 km/h
 GOOD LUCK 😀
 */
 
+class Car {
+    constructor(make,speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+
+    accelerate() {
+        this.speed += 10;
+        console.log(`${this.make} is going at ${this.speed} km/h`);
+    }
+    
+    brake() {
+        this.speed -= 5;
+        console.log(`${this.make} is going at ${this.speed} km/h`);
+    }
+
+    get speedUS(){
+        return this.speed / 1.6;
+    }
+
+    set speedUS(speed){
+        this.speed = speed * 1.6;
+    }
+}
+
+const ford = new Car('Ford', 120);
+console.log(ford);
+console.log(ford.speedUS);
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+ford.speedUS = 50;
+console.log(ford);
